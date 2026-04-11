a52046f9e2ea20d5ad38303bab6976eb0c9b6cb3
dynl <johnyoyd@gmail.com>
2026-04-11 23:35:36 +0800

    Spikemen

--- script/hazards.gd
+++ script/hazards.gd
@@ -1,8 +1,22 @@
-extends Area2D
+extends Node2D
 
-func _on_Spike_body_entered(body):
-	if body is KinematicBody2D:
-		var gs = get_tree().get_root().find_node("GameState", true, false)
-		if gs:
-			gs.player_hurt(body)
-			print("Spike hit the player!")
+func _on_Area2Dspike_body_entered(body):
+		if body is KinematicBody2D:
+			var gs = get_tree().get_root().find_node("GameState", true, false)
+			if gs:
+				gs.player_hurt(body)
+				print("Spike hit the player!")
+
+
+
+
+
+
+
+
+func _on_Area2DSpikemen_body_entered(body):
+		if body is KinematicBody2D:
+			var gs = get_tree().get_root().find_node("GameState", true, false)
+			if gs:
+				gs.player_hurt(body)
+				print("Spike hit the player!")
